cad42840cb40d039d34d3a35187757c9b10d4614
VictordalaM <delamotte.v@gamil.com>
2023-12-22 17:57:21 +0100

    variablen verschoben

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,13 @@ let text = document.querySelector("#text")
 let boxArr = Array.from(document.querySelectorAll(".box"))
 let output = document.querySelector("#output")
 let button = document.querySelector("input")
-
-function createRandom(max){
+let rightColor = boxArr[Math.ceil(createRandomWithMax(5))].style.backgroundColor
+text.innerHTML = `guess what color ${rightColor} is:`
+function createRandomWithMax(max){
     return max * Math.random() 
 }
 for (let i = 0; i < boxArr.length; i++){
-    boxArr[i].style.backgroundColor = `rgb(${createRandom(256)},${createRandom(256)},${createRandom(256)})`
+    boxArr[i].style.backgroundColor = `rgb(${createRandomWithMax(256)},${createRandomWithMax(256)},${createRandomWithMax(256)})`
     boxArr[i].addEventListener("click", () => {
         switch(boxArr[boxArr.indexOf(event.target)].style.backgroundColor === rightColor){
             case true:
@@ -19,14 +20,13 @@ for (let i = 0; i < boxArr.length; i++){
         }
     })
 }
-let rightColor = boxArr[Math.ceil(createRandom(5))].style.backgroundColor
-text.innerHTML = `guess what color ${rightColor} is:`
+
 
 
 button.addEventListener("click", () => {
     for (let i = 0; i < boxArr.length; i++){
-        rightColor = boxArr[Math.ceil(createRandom(5))].style.backgroundColor
-        boxArr[i].style.backgroundColor = `rgb(${createRandom(256)},${createRandom(256)},${createRandom(256)})`
+        rightColor = boxArr[Math.ceil(createRandomWithMax(5))].style.backgroundColor
+        boxArr[i].style.backgroundColor = `rgb(${createRandomWithMax(256)},${createRandomWithMax(256)},${createRandomWithMax(256)})`
         boxArr[i].addEventListener("click", () => {
             switch(boxArr[boxArr.indexOf(event.target)].style.backgroundColor === rightColor){
                 case true:
